7c072af9d0cfc5fcf06de52f9ec75a75df73a9dc
ProjectPB <73194192+ProjectPB@users.noreply.github.com>
2021-06-16 22:00:49 +0200

    Connect sponsors to firestore

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -1,27 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { db } from "../../firebase";
 
 function Sponsors() {
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        db.collection("sponsors")
+            .get()
+            .then((querySnapshot) =>
+                setImages(querySnapshot.docs.map((doc) => doc.data()))
+            );
+    }, []);
+
     return (
         <SponsorsContainer id="sponsors">
             <Typography>Sponsors</Typography>
             <ImagesContainer>
-                <Image
-                    src="https://image.flaticon.com/icons/png/512/546/546490.png"
-                    alt=""
-                />
-                <Image
-                    src="https://image.freepik.com/free-vector/fashion-logo_23-2148558723.jpg"
-                    alt=""
-                />
-                <Image
-                    src="https://image.freepik.com/free-vector/simple-spartan-vector-template-logo-design_115591-16.jpg"
-                    alt=""
-                />
-                <Image
-                    src="https://image.freepik.com/free-vector/logo-with-house-wave_1017-1013.jpg"
-                    alt=""
-                />
+                {images.map((item) => (
+                    <Image src={item.url} alt="" />
+                ))}
             </ImagesContainer>
         </SponsorsContainer>
     );
@@ -31,12 +29,11 @@ const SponsorsContainer = styled.div`
     padding: 10px;
 `;
 
-const Typography = styled.h3`
+const Typography = styled.h1`
     width: 100%;
+    color: white;
     text-align: center;
     font-size: 20px;
-    font-weight: 400;
-    display: block;
     margin-bottom: 10px;
 `;
 
@@ -44,15 +41,16 @@ const ImagesContainer = styled.div`
     width: 100%;
     margin: 0 auto;
     display: flex;
-    padding: 5px;
     flex-wrap: wrap;
     justify-content: center;
+    align-items: center;
 `;
 
 const Image = styled.img`
-    margin: 0 30px;
-    width: 100px;
+    margin: 15px 30px;
+    height: 75px;
     object-fit: contain;
+    cursor: pointer;
 `;
 
 export default Sponsors;
